a569026cd2f36d5239d5319f1a0fbbe04fef1fdc
arnavkj1995 <arnavkj95@gmail.com>
2017-01-25 17:01:00 +0530

    Adding File Assignment

---
+++ Lab 3/pe.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <fcntl.h>
+main() {
+	int fd1, fd2;
+	char c;
+	fd1 = open("xyz", O_RDONLY);
+	fd2 = open("pqr", O_WRONLY);
+	lseek(fd1, 6, SEEK_SET);
+	read(fd1, &c, 1);
+	printf("%c \n", c);
+	write(fd2, &c, 1);
+}
